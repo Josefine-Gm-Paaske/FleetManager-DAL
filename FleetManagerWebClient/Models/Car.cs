15ecc8fc696da9a6eac4887d75eab30cb5f2568f
Josefine Paaske <1086237@ucn.dk>
2021-11-02 13:42:24 +0100

    02-11-2021 Programmering

---
+++ FleetManagerWebClient/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FleetManagerWebClient.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public int Mileage { get; set; }
+        public DateTime Date { get; set; }
+        public Location Location { get; set; }
+    }
+}
