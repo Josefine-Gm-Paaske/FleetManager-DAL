3582f8e5bc3ff7ba518c9fcc24d169e652663097
Josefine Paaske <1086237@ucn.dk>
2021-11-02 10:28:23 +0100

    Rest Dao created

---
+++ FleetManager.DataAccessLayer/Daos/Rest/CarDao.cs
@@ -0,0 +1,79 @@
+﻿using FleetManager.DataAccessLayer.Daos.Rest.Model;
+using FleetManager.Entities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.DataAccessLayer.Daos.Rest
+{
+    class CarDao : BaseDao<IDataContext<IRestClient>>, IDao<Car>
+    {
+        public CarDao(IDataContext<IRestClient> dataContext) : base(dataContext)
+        {
+        }
+
+        public Car Create(Car model)
+
+        {
+            IRestClient client = DataContext.Open();
+
+            IRestRequest request = new RestRequest("/api/Cars", Method.POST);
+            request.AddParameter("Brand", model.Brand);
+            request.AddParameter("Mileage", model.Mileage);
+
+            IRestResponse<IEnumerable<CarDto>> response = client.Post<IEnumerable<CarDto>>(request);
+
+            return model;
+        }
+
+        public bool Delete(Car model)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Cars/{id}", Method.DELETE);
+            request.AddUrlSegment("id", model.Id);
+            IRestResponse<IEnumerable<CarDto>> response = client.Post<IEnumerable<CarDto>>(request);
+
+            return false;
+        }
+
+        public IEnumerable<Car> ReadAll()
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Cars", Method.GET);
+            IRestResponse<IEnumerable<CarDto>> response = client.Get<IEnumerable<CarDto>>(request);
+            List<Car> result = new();
+            foreach (CarDto car in response.Data)
+            {
+                result.Add(car.Map());
+            }
+            return result;
+        }
+
+
+        public IEnumerable<Car> ReadbyId(Func<Car, bool> predicate)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Cars", Method.GET);
+            IRestResponse<IEnumerable<CarDto>> response = client.Get<IEnumerable<CarDto>>(request);
+            List<Car> result = new();
+            foreach (CarDto car in response.Data)
+            {
+                result.Add(car.Map());
+            }
+            return result.Where(predicate);
+        }
+
+        public bool Update(Car model)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Cars/{id}", Method.PUT);
+            request.AddUrlSegment("id", model.Id);
+            client.Put<Car>(request);
+            return true;
+        }
+    }
+}
+
---
+++ FleetManager.DataAccessLayer/Daos/Rest/LocationDao.cs
@@ -0,0 +1,82 @@
+﻿using FleetManager.DataAccessLayer.Daos.Rest.Model;
+using FleetManager.Entities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.DataAccessLayer.Daos.Rest
+{
+    class LocationDao : BaseDao<IDataContext<IRestClient>>, IDao<Location>
+    {
+        public LocationDao(IDataContext<IRestClient> dataContext) : base(dataContext)
+        {
+        }
+
+        public Location Create(Location model)
+        {
+            IRestClient client = DataContext.Open();
+
+            IRestRequest request = new RestRequest("/api/Locations", Method.POST);
+            request.AddParameter("Name", model.Name);
+
+            IRestResponse<IEnumerable<LocationDto>> response = client.Post<IEnumerable<LocationDto>>(request);
+
+            return model;
+        }
+
+        public bool Delete(Location model)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Locations/{id}", Method.DELETE);
+            request.AddUrlSegment("id", model.Id);
+
+            //IRestResponse response = client.Delete(request);
+            IRestResponse<IEnumerable<LocationDto>> response = client.Delete<IEnumerable<LocationDto>>(request);
+
+            return false;
+        }
+
+        public IEnumerable<Location> ReadAll()
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Locations", Method.GET);
+            IRestResponse<IEnumerable<LocationDto>> response = client.Get<IEnumerable<LocationDto>>(request);
+            List<Location> result = new();
+            foreach (LocationDto location in response.Data)
+            {
+                result.Add(location.Map());
+            }
+            return result;
+        }
+
+        public IEnumerable<Location> ReadbyId(Func<Location, bool> predicate)
+        {
+            IRestClient client = DataContext.Open();
+            IRestRequest request = new RestRequest("/api/Locations", Method.GET);
+            IRestResponse<IEnumerable<LocationDto>> response = client.Get<IEnumerable<LocationDto>>(request);
+            List<Location> result = new();
+            foreach (LocationDto location in response.Data)
+            {
+                result.Add(location.Map());
+            }
+            return result.Where(predicate);
+        }
+
+        public bool Update(Location model)
+        {
+            IRestClient client = DataContext.Open();
+
+            IRestRequest request = new RestRequest("/api/location/{id}", Method.PUT);
+            request.AddUrlSegment("id", model.Id);
+            request.AddParameter("Name", model.Name);
+
+            IRestResponse<IEnumerable<LocationDto>> response = client.Post<IEnumerable<LocationDto>>(request);
+
+            return false; //TODO Should return from correct response currently only returns false
+        }
+    }
+}
+
