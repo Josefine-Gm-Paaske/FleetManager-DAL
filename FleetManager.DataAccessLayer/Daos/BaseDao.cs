287257b8c13b61f4f66dbe82624e28ee7e18a911
Josefine Paaske <1086237@ucn.dk>
2021-11-02 10:29:14 +0100

    Fixed mistake. Wrong generic type

--- FleetManager.DataAccessLayer/Daos/BaseDao.cs
+++ FleetManager.DataAccessLayer/Daos/BaseDao.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FleetManager.DataAccessLayer.Daos
 {
-    internal abstract class BaseDao<TModel> : IDao<TModel>
+    ///<typeparam name="TDataContext"></typeparam>
+    class BaseDao<TDataContext> : IDataContext
     {
-        public IDataContext DataContext { get; }
+        public TDataContext DataContext { get; }
 
-        public BaseDao(IDataContext dataContext)
+        public BaseDao(TDataContext dataContext)
         {
             DataContext = dataContext;
         }
 
-        public abstract IEnumerable<TModel> ReadAll();
-        public abstract TModel ReadById(int id);
-        public abstract int Create(TModel model);
-        public abstract int Update(TModel model);
-        public abstract int Delete(TModel model);
+        //public abstract IEnumerable<TModel> ReadAll();
+        //public abstract TModel ReadById(int id);
+        //public abstract int Create(TModel model);
+        //public abstract int Update(TModel model);
+        //public abstract int Delete(TModel model);
     }
 }
